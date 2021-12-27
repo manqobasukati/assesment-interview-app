47163f87e27bdac0190d6646808d9feb3ca7909e
manqobasukati <manqoba.sukati@peoplesized.com>
2021-12-27 15:14:11 +0200

    handlers for selectors and changers

--- pages/relationships/index.tsx
+++ pages/relationships/index.tsx
@@ -1,10 +1,18 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
 import Navigation from '../../components/navigation';
 import { people } from '../people';
 
 const Relationships: NextPage = () => {
-  const first = Object.keys(people);
-  console.log(first);
+  const persons = Object.keys(people);
+
+  const [fisrtPerson, setFirstPerson] = useState();
+  const [secondPerson, setSecondPerson] = useState();
+
+  const handleButtonCheck = () => {
+    console.log(fisrtPerson, secondPerson);
+  };
+
   return (
     <div className=" flex items-center justify-center  h-screen w-full">
       <Navigation />
@@ -12,9 +20,17 @@ const Relationships: NextPage = () => {
         <div className="text-lg font-semibold">Check relationship between</div>
         <div className="flex items-center gap-4">
           <div className="flex flex-col">
-            <label className='text-left' htmlFor="friend_1">Friend 1</label>
-            <select className="border-purple-200  border-2 p-2 rounded-md focus:outline-none">
-              {first.map((val, index) => {
+            <label className="text-left" htmlFor="friend_1">
+              Friend 1
+            </label>
+            <select
+              onChange={(e: any) => {
+                const name = e.target.value;
+                setFirstPerson(name);
+              }}
+              className="border-purple-200  border-2 p-2 rounded-md focus:outline-none"
+            >
+              {persons.map((val, index) => {
                 return (
                   <option value={val} key={index}>
                     {val}
@@ -28,9 +44,17 @@ const Relationships: NextPage = () => {
             <span className="material-icons text-purple-400">close</span>
           </div>
           <div className="flex flex-col">
-            <label className='text-left' htmlFor="friend_1">Friend 1</label>
-            <select className="border-purple-200  border-2 p-2 rounded-md focus:outline-none">
-              {first.map((val, index) => {
+            <label className="text-left" htmlFor="friend_1">
+              Friend 2
+            </label>
+            <select
+              onChange={(e: any) => {
+                const name = e.target.value;
+                setSecondPerson(name);
+              }}
+              className="border-purple-200  border-2 p-2 rounded-md focus:outline-none"
+            >
+              {persons.map((val, index) => {
                 return (
                   <option value={val} key={index}>
                     {val}
@@ -40,7 +64,9 @@ const Relationships: NextPage = () => {
             </select>
           </div>
         </div>
-        <button className='bg-purple-200 p-2 text-white rounded-sm'>Check</button>
+        <button onClick={handleButtonCheck} className="bg-purple-200 p-2 text-white rounded-sm">
+          Check
+        </button>
         <div className="text-gray-500">Degree of seperation</div>
       </div>
     </div>
